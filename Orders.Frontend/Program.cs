e99c7b4bbb5b16b09a944c2fb6ef9316ca4a91ca
Lorena1977 <137176422+Lorena1977@users.noreply.github.com>
2025-10-26 21:50:33 +0100

    Add edit users

--- Orders.Frontend/Program.cs
+++ Orders.Frontend/Program.cs
@@ -3,6 +3,7 @@ using MudBlazor.Services;
 using Orders.Frontend.AuthenticationProviders;
 using Orders.Frontend.Components;
 using Orders.Frontend.Repositories;
+using Orders.Frontend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,10 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+
 
 var app = builder.Build();
 
